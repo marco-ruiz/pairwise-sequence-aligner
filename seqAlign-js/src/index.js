899f26d5d095fded6801d38dba419153afcc8403
Marco Ruiz <marco.a.ruiz@gmail.com>
2019-04-16 19:10:29 -0700

    Minor tweaks to integrate React app with Spring boot based web service

--- seqAlign-js/src/index.js
+++ seqAlign-js/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import AppPage from './components/AppPage';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<AppPage apiUrl='http://localhost:8080/api/align'/>, document.getElementById('root'));
+ReactDOM.render(<AppPage apiUrl='/api/align'/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
